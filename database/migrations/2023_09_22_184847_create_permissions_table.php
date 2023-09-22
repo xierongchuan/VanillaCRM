f280773464cb7350ff2ac60a700a2723b17bb776
テムルマル <86427523+Temurumaru@users.noreply.github.com>
2023-09-23 01:08:00 +0500

    Созданы миграции для БД и работа с клавиатурой

---
+++ database/migrations/2023_09_22_184847_create_permissions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+				Schema::create('permissions', function (Blueprint $table) {
+						$table->id();
+						$table->unsignedBigInteger('com_id');
+						$table->string('name');
+						$table->string('value');
+						$table->timestamps();
+
+						// Добавляем внешний ключ
+						$table->foreign('com_id')->references('id')->on('companies');
+				});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permissions');
+    }
+};
---
+++ database/migrations/2023_09_22_185105_create_workers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+				Schema::create('workers', function (Blueprint $table) {
+						$table->id();
+						$table->unsignedBigInteger('com_id');
+						$table->unsignedBigInteger('post_id');
+						$table->string('full_name');
+						$table->string('phone_number');
+						$table->timestamps();
+
+						// Добавляем внешний ключ к таблице companies
+						$table->foreign('com_id')->references('id')->on('companies');
+
+						// Добавляем внешний ключ к таблице posts
+						$table->foreign('post_id')->references('id')->on('posts');
+				});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workers');
+    }
+};
