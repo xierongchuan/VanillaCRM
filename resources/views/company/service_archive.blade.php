fcb2ddb4fb2511660dae8f4f9419820770e53fbd
テムルマル <86427523+Temurumaru@users.noreply.github.com>
2024-03-06 22:51:51 +0500

    ЭТАП 1  завершён на 99%

---
+++ resources/views/company/service_archive.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.main')
+
+@section('title', 'Архив Сервис')
+
+@section('nav_right')
+    <li class="nav-item">
+        <a class="btn btn-danger" aria-current="page"
+           href="">Удалить последний отчёт</a>
+    </li>
+@endsection
+
+@section('content')
+    <h1 class="text-center my-2">
+        Архив Сервис {{$company -> name}}
+    </h1>
+
+    <div class="flex-column align-items-center">
+        <div class="bg-body-tertiary rounded p-3 mb-2">
+
+            <div class="my-1 m-auto border rounded py-2 row h4">
+
+                <div class="col-6">
+                    Дата
+                </div>
+
+                <div class="col-6 text-end">
+                    Всего
+                </div>
+
+            </div>
+
+            @foreach($reports as $key => $val)
+
+                <div class="my-1 m-auto border rounded py-2 row h4">
+
+                    <div class="col-6 h5">
+                        <a href="{{ route('company.service.archive', [$company, $val[0]]) }}">{{$key}}</a>
+                    </div>
+
+
+                    <div class="col-6 text-end">
+                        {{$val[1]}}
+                    </div>
+                </div>
+
+            @endforeach
+
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/user/permissions/report_service.blade.php
@@ -0,0 +1,41 @@
+<div class="row flex-column align-items-center">
+	<div class="col-lg-9 bg-body-secondary rounded mt-3 p-2">
+		<span class="d-flex justify-content-between">
+			<h2 class="perm_panel_switch mx-1" panel="perm_panel_create_worker">Отчёт сервис в <b>{{$data -> company -> name}}</b></h2>
+			<button class="lead perm_panel_switch m-1" panel="perm_panel_create_worker"><i class="bi bi-nintendo-switch"></i></button>
+		</span>
+		<form id="perm_panel_create_worker" action="{{route('mod.report_service', $data -> company -> id)}}" method="post" class="perm-panel-list bg-body-tertiary rounded p-3">
+			@csrf
+
+			<div class="form-group mb-2">
+				<label for="date">Дата:</label>
+				<input type="date" class="form-control" id="date" name="date" value="{{old('date') ?? date("Y-m-d")}}" required>
+			</div>
+			<div class="form-group mb-2">
+				<label for="dop">Доп:</label>
+				<input type="number" min="0" class="form-control" id="dop" name="dop" value="{{old('dop')}}" required>
+			</div>
+			<div class="form-group mb-2">
+				<label for="now">Текущий:</label>
+				<input type="number" min="0" class="form-control" id="now" name="now" value="{{old('now')}}" required>
+			</div>
+			<div class="form-group mb-2">
+				<label for="to">ТО:</label>
+				<input type="number" min="0" class="form-control" id="to" name="to" value="{{old('to')}}" required>
+			</div>
+
+			<div class="form-group mb-2">
+				<label for="kuz">Кузовной:</label>
+				<input type="number" min="0" class="form-control" id="kuz" name="kuz" value="{{old('kuz')}}" required>
+			</div>
+
+			<div class="form-group mb-2">
+				<label for="store">Магазин:</label>
+				<input type="number" min="0" class="form-control" id="store" name="store" value="{{old('store')}}" required>
+			</div>
+			<div class="d-flex justify-content-center">
+				<button type="submit" class="btn btn-primary">Создать</button>
+			</div>
+		</form>
+	</div>
+</div>
