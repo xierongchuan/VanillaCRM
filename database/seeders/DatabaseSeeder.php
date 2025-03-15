423a7e823790324569958eb9b1e5d013d49d5b29
谢榕川 <temurumaru@yandex.com>
2025-03-16 01:10:00 +0800

    Обновление зависимостей Laravel и исправление фабрики пользователей для улучшения безопасности

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,10 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,18 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
         User::factory()->create([
-					 'login' => 'admin',
-					 'role' => 'admin',
-					 'password' => function() {
-						print 'Введите пароль Администратора';
-						return Hash::make(fgets(STDIN));
-					 },
-					 'remember_token' => null,
-					 'created_at' => '2023-09-24 12:26:05',
-					 'updated_at' => '2023-09-24 12:26:05',
+            'login' => 'admin',
+            'role' => 'admin',
+            'password' => function() {
+                print 'Введите пароль Администратора: ';
+                return Hash::make(trim(fgets(STDIN)));
+            },
+            'remember_token' => null,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
     }
 }
