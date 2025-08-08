c162456aff235bf4a1e7275898c2146578980108
谢榕川 <xierongchuan@tutamail.com>
2025-08-08 23:15:00 +0500

    fix: unfound name to login

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -18,7 +18,7 @@ class UserResource extends JsonResource
     {
         return [
             'id'           => $this->id,
-            'name'         => $this->name,
+            'login'         => $this->login,
             'role'         => $this->role,
             'full_name'    => $this->full_name,
             'phone_number' => $this->phone_number,
