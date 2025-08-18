2e1d7b0178d8e8549204ab903903dc725b9ccd2b
谢榕川 <xierongchuan@tutamail.com>
2025-08-18 08:49:55 +0500

    Fix MySQL regex format from PostgreSQL regex format

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,6 +19,9 @@ class UserResource extends JsonResource
         return [
             'id'           => $this->id,
             'login'        => $this->login,
+            // TODO: заменить заглушку на реальные данные
+            'role'         => 'user',
+            // 'role'         => $this->bot_role,
             'status'       => $this->status,
             'full_name'    => $this->full_name,
             'phone_number' => $this->phone_number,
