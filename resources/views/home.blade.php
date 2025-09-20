5632f03bf0032ef82720bd0d3cded4be91150bec
谢榕川 <xierongchuan@tutamail.com>
2025-09-20 23:59:36 +0500

    style(home): improve display of turnover values with color coding
    - Replace separate "Oborot Plus" and "Oborot Minus" rows with a single "Oborot" row
    - Display positive and negative turnover values side by side with green and red text
    - Apply the same style to both count and sum turnover columns
    - Use flexbox layout for responsive alignment and spacing
    - Enhance visual clarity and reduce row clutter in the turnover section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -946,24 +946,38 @@
                                                     @endif
                                                 </td>
                                             </tr>
+
                                             <tr>
-                                                <td>Оборот Плюс</td>
-                                                <td class="text-nowrap overflow-hidden text-end">
-                                                    {{ number_format($cashier_reps[$company->id]['oborot_plus'], 0, '', ' ') }}
+                                                <td>Оборот</td>
+
+                                                <td class="text-end">
+                                                    <div
+                                                        class="d-flex gap-3 justify-content-end flex-column flex-md-row align-items-end">
+                                                        <div class="text-nowrap text-success">
+                                                            {{ number_format($cashier_reps[$company->id]['oborot_plus'], 0, '', ' ') }}
+                                                        </div>
+                                                        <div class="text-nowrap">|</div>
+                                                        <div class="text-nowrap text-danger">
+                                                            {{ number_format($cashier_reps[$company->id]['oborot_minus'], 0, '', ' ') }}
+                                                        </div>
+                                                    </div>
                                                 </td>
-                                                <td class="text-nowrap overflow-hidden text-end">
-                                                    {{ number_format($cashier_reps[$company->id]['oborot_plus_sum'], 0, '', ' ') }}
+
+                                                <td class="text-end">
+                                                    <div
+                                                        class="d-flex gap-3 justify-content-end flex-column flex-md-row align-items-end">
+                                                        <div class="text-nowrap text-success">
+                                                            {{ number_format($cashier_reps[$company->id]['oborot_plus_sum'], 0, '', ' ') }}
+                                                        </div>
+                                                        <div class="text-nowrap">|</div>
+                                                        <div class="text-nowrap text-danger">
+                                                            {{ number_format($cashier_reps[$company->id]['oborot_minus_sum'], 0, '', ' ') }}
+                                                        </div>
+                                                    </div>
                                                 </td>
                                             </tr>
-                                            <tr>
-                                                <td>Оборот Минус</td>
-                                                <td class="text-nowrap overflow-hidden text-end">
-                                                    {{ number_format($cashier_reps[$company->id]['oborot_minus'], 0, '', ' ') }}
-                                                </td>
-                                                <td class="text-nowrap overflow-hidden text-end">
-                                                    {{ number_format($cashier_reps[$company->id]['oborot_minus_sum'], 0, '', ' ') }}
-                                                </td>
-                                            </tr>
+
+
                                             <tr>
                                                 <td>Сальдо</td>
                                                 <td class="text-nowrap overflow-hidden text-end">
